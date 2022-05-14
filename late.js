b055c5999bd0fe8dbcd437995b765a2cbf61018c
noclouthere <40571849+noclouthere@users.noreply.github.com>
2022-05-13 22:26:14 -0600

    Added functionality.

--- late.js
+++ late.js
@@ -1,1 +1,12 @@
-// start coding your function here!
+export default function running_late(date){
+
+    var expectedTime = ("10:00:00");
+
+    if(date > expectedTime){
+        console.log("It is late!");
+    }
+    else{
+        console.log("It is still early!");
+    }
+}
+
