944735ce546922ef94f4e331d57802a8414d23be
noclouthere <40571849+noclouthere@users.noreply.github.com>
2022-05-14 07:02:17 -0600

    Added functionality. Should return the maximum number in an array.

--- max_nums.js
+++ max_nums.js
@@ -1,1 +1,13 @@
-// start coding your function here!
+export default function find_max(arr){
+    
+    var arrayCopy = arr.toArray();
+    let maxNum = 0;
+
+    for(let i = 0; i < arrayCopy.length() - 1; i++){
+        if(arrayCopy[i] > arrayCopy[i+1]){
+            maxNum = arrayCopy[i];
+        }
+    }
+
+    return maxNum;
+}
